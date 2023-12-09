50f43f7847ea7ddd590fbdb6d9c8b8582d35148b
kasunJKD <kasun.ravihara@gmail.com>
2023-12-09 15:03:54 +0530

    day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
 use std::io::{self};
 use std::io::BufRead;
+use std::thread::current;
 use regex::Regex;
+use std::collections::HashMap;
 
 fn main()->io::Result<()> {
    // 1abc2
@@ -19,11 +21,43 @@ fn main()->io::Result<()> {
 
     let mut total = 0;
     let re = Regex::new(r"\d").unwrap();
+    // for line in reader.lines() {
+    //     let line = line?;
+    //     let numbers: Vec<i32> = re.find_iter(&line)
+    //                                     .filter_map(|m| m.as_str().parse().ok())
+    //                                     .collect();
+
+    //     if numbers.len() >= 2 {
+    //         let first_number = *numbers.get(0).unwrap_or(&0);
+    //         let last_number = *numbers.get(numbers.len() - 1).unwrap_or(&0);
+
+    //         let combined_number = first_number * 10 + last_number;
+    //         println!("Combined number: {}", combined_number);
+    //         total += combined_number;
+    //     } else {
+    //         let combined_number = numbers[0] * 10 + numbers[0];
+    //         println!("Combined for 1: {}", combined_number);
+    //         total += combined_number;
+    //     }
+
+    //     println!("Numbers in line: {:?}", numbers);
+    // }
+
     for line in reader.lines() {
+        let mut line_val: Vec<String> = Vec::new();
         let line = line?;
-        let numbers: Vec<i32> = re.find_iter(&line)
-                                        .filter_map(|m| m.as_str().parse().ok())
-                                        .collect();
+        let parts: Vec<String> = split_string(&line);
+
+        for p in parts {
+            let val = check_for_word(p.as_str());
+            line_val.push(val);
+            
+        }
+        println!("partval: {:?}", line_val);
+
+        let numbers: Vec<i32> = re.find_iter(&line_val.join(""))
+                                    .filter_map(|m| m.as_str().parse().ok())
+                                    .collect();
 
         if numbers.len() >= 2 {
             let first_number = *numbers.get(0).unwrap_or(&0);
@@ -39,10 +73,57 @@ fn main()->io::Result<()> {
         }
 
         println!("Numbers in line: {:?}", numbers);
+
     }
 
-    println!("Total: {}", total);
+    println!("{:?}", total);
 
     Ok(())
 
+}
+
+fn check_for_word(input: &str) -> String {
+    let words: HashMap<String, &str> = HashMap::from([
+        ("one".to_string(), "1"),
+        ("two".to_string(), "2"),
+        ("three".to_string(), "3"),
+        ("four".to_string(), "4"),
+        ("five".to_string(), "5"),
+        ("six".to_string(), "6"),
+        ("seven".to_string(), "7"),
+        ("eight".to_string(), "8"),
+        ("nine".to_string(), "9"),
+    ]);
+
+    let mut replaced = input.to_string();
+    for word in words.keys() {
+        if input.contains(word) {
+            replaced = replaced.replace(word, words.get(word).unwrap());
+        }
+    }
+
+    replaced
+}
+
+fn split_string(s: &str) -> Vec<String> {
+    let mut parts = Vec::new();
+    let mut current_part = String::new();
+
+    for c in s.chars() {
+        if c.is_numeric() {
+            if !current_part.is_empty() {
+                parts.push(current_part.clone());
+                current_part.clear();
+            }
+            parts.push(c.to_string());
+        } else {
+            current_part.push(c);
+        }
+    }
+
+    if !current_part.is_empty() {
+        parts.push(current_part);
+    }
+
+    parts
 }
